9e679ca95d4e75ec3011ebbdc65aba0dd73a7717
Sean McLaughlin <mclaughlin6464@gmail.com>
2016-03-10 15:36:17 -0800

    fixed do_call

--- src/do_call.py
+++ src/do_call.py
@@ -1,25 +1,29 @@
 #@Author Chris Davis
 #Send a random batch of exposures to ki-ls
+#optionally, specify the expids from the command line
 from __future__ import print_function, division
 import numpy as np
 import pandas as pd
 from subprocess import call
 from os import path
+from sys import argv
 
 jamierod_results_path = '/nfs/slac/g/ki/ki18/des/cpd/jamierod_results.csv'
 jamierod_results = pd.read_csv(jamierod_results_path)
-
-# choose a random 40 expids from jamierod results
-indx_choice = np.random.choice(len(jamierod_results), 40)
-expids = jamierod_results.iloc[indx_choice]['expid']
-
-expids = list(expids)
-expids.append(149440)
 
 #out_dir = '/nfs/slac/g/ki/ki18/des/cpd/DeconvOutput'
 out_dir = '/nfs/slac/g/ki/ki18/des/swmclau2/DeconvOutput'
 #code_path = '/nfs/slac/g/ki/ki18/cpd/Projects/WavefrontPSF/code/DeconvolvePSF/afterburner.py'
 code_path = '/u/ki/swmclau2/Git/DeconvolvePSF/DeconvolvePSF/afterburner.py'
+
+if len(argv) == 1: #do a random collection of 40
+    # choose a random 40 expids from jamierod results
+    indx_choice = np.random.choice(len(jamierod_results), 40)
+    expids = jamierod_results.iloc[indx_choice]['expid']
+
+    expids = list(expids)
+else:
+    expids = [int(expid) for expid in sys[1:] ]
 
 req_time = 240 # minutes
 
